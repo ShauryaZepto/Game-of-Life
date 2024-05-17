6efc1f72fee1e0b37eefa18f0c244a429885b76d
Shaurya Priyadarshi <shaurya@prakhars-mac.local>
2024-05-17 07:37:46 +0530

    feat: implemented count_live_neighbours in Neighbours.go

--- gameoflife/Neighbours.go
+++ gameoflife/Neighbours.go
@@ -1,5 +1,18 @@
 package gameoflife
 
 func (B *Board) count_live_neighbours(x int, y int) int {
-
+	count := 0
+	for i:= x-1; i<=x+1: i++ {
+		for j:= y-1; j<=y+1; j++{
+			if(i==x&&j==y){
+				continue
+			}
+			if(check_valid_index(i,j)){
+				if(B.grid[i][j]){
+					count++
+				}
+			}
+		}
+	}
+	return count
 }
