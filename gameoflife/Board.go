012130e84cd6296dd38ff60b69a1594cb92b8a3a
Shaurya Priyadarshi <shaurya@prakhars-mac.local>
2024-05-17 09:22:42 +0530

    feat: implemeneted Set_Board in board.go

--- gameoflife/Board.go
+++ gameoflife/Board.go
@@ -18,6 +18,14 @@ func CreateBoard(rows int, cols int) *Board {
 	return board
 }
 
+func (B *Board) Set_Board(matrix [][]bool, rows int, cols int) {
+	for i := range rows {
+		for j := range cols {
+			B.grid[i][j] = matrix[i][j]
+		}
+	}
+}
+
 func (B *Board) Check_Valid_Index(x int, y int) bool {
 	if x < 0 || x >= B.rows || y < 0 || y >= B.cols {
 		return false
